b891d10b40cc910489f652ffdfb300ca76cb3ac4
quest <quest@mac.com>
2026-07-30 13:09:46 -1000

    Bump version to 1.6.0

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 # shellcheck disable=SC2086,SC2001
 # Celestial GTK Theme Installer
-# Version: 1.5.2
+# Version: 1.6.0
 
 ROOT_UID=0
 DEST_DIR=
@@ -64,7 +64,7 @@ THEME_VARIANTS=('-sea' '-aliz' '-azul' '-pueril')
 SHELL_VERSION=""
 
 usage() {
-  printf "%s\n" "Celestial GTK Theme Installer v1.5.2"
+  printf "%s\n" "Celestial GTK Theme Installer v1.6.0"
   printf "%s\n" "Usage: $0 [OPTIONS...]"
   printf "\n%s\n" "OPTIONS:"
   printf "  %-25s%s\n" "-d, --dest DIR" "Destination directory (Default: ${DEST_DIR})"
